e36da3907f4e4e9852d4688c6939578a92a65a21
bonecrushereb <bonecrushereb@gmail.com>
2016-12-23 00:18:27 -0800

    add course and review classes with interfaces

---
+++ course-reviews-api/src/main/java/com/teamtreehouse/course/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.teamtreehouse.course;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by BenNolan on 12/23/16.
+ */
+public interface CourseRepository extends CrudRepository<Course, Long> {
+
+}
