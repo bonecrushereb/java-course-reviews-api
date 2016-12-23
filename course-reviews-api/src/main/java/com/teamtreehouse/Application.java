ea8ec38b1376abf288fd77e8a41b9491b45a57a7
bonecrushereb <bonecrushereb@gmail.com>
2016-12-23 12:04:39 -0800

    add application server

---
+++ course-reviews-api/src/main/java/com/teamtreehouse/Application.java
@@ -0,0 +1,14 @@
+package com.teamtreehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by BenNolan on 12/23/16.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
